7b311b5ff82e17a4f00ccf1826000bb2fdeb23bd
Yoshinori.N <nayo-msoft@outlook.jp>
2016-07-23 23:40:00 +0900

    (Issue #3) : Create DelegateCommand class on Kinugasa.Mvvm.Core.csproj.

---
+++ Source/Mvvm/Kinugasa.Mvvm.Core/DelegateCommand.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Windows.Input;
+
+/// <summary>
+/// Provide mvvm infrastructure.
+/// </summary>
+namespace Kinugasa.Mvvm.Core
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class DelegateCommand : ICommand
+    {
+        private readonly Action _execute;
+        private readonly Func<bool> _canExecute;
+
+        public event EventHandler CanExecuteChanged;
+
+        public DelegateCommand(Action execute) : this(execute, () => true) { }
+
+        public DelegateCommand(Action execute, Func<bool> canExecute)
+        {
+            if (execute == null)
+            {
+                throw new ArgumentException("execute");
+            }
+
+            this._execute = execute;
+            this._canExecute = canExecute;
+        }
+
+        public void Execute(object parameter)
+        {
+            this._execute();
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null ? true : _canExecute();
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            var handler = CanExecuteChanged;
+            if (handler != null)
+            {
+                handler(this, EventArgs.Empty);
+            }
+        }
+    }
+
+    public class DelegateCommand<T> : ICommand
+    {
+
+        private readonly Action<T> _execute;
+        private readonly Func<T, bool> _canExecute;
+
+        public event EventHandler CanExecuteChanged;
+
+        public DelegateCommand(Action<T> execute) : this(execute, (O) => true) { }
+
+        public DelegateCommand(Action<T> execute, Func<T, bool> canExecute)
+        {
+            if (execute == null)
+            {
+                throw new ArgumentException("execute");
+            }
+
+            this._execute = execute;
+            this._canExecute = canExecute;
+        }
+
+        public void Execute(object parameter)
+        {
+            this._execute((T)parameter);
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null ? true : _canExecute((T)parameter);
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            var handler = CanExecuteChanged;
+            if (handler != null)
+            {
+                handler(this, EventArgs.Empty);
+            }
+        }
+    }
+}
