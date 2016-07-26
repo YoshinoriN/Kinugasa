8572940320d049acd1a059f6360effcf4fb92ca7
Yoshinori.N <nayo-msoft@outlook.jp>
2016-07-27 02:02:22 +0900

    (Issue #10) : Add 'DelegateCommand' class test code.

---
+++ Test/Mvvm/Mvvm.Test/DelegateCommandTest.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Reflection;
+using System.Windows.Input;
+
+namespace Mvvm.Test
+{
+    [TestClass]
+    public class DelegateCommandTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(System.ArgumentException))]
+        public void ArgumentExceptionTest()
+        {
+            var command = new Kinugasa.Mvvm.DelegateCommand(null);
+        }
+
+        [TestMethod]
+        public void CanExecuteNullTest()
+        {
+            var command = new Kinugasa.Mvvm.DelegateCommand(Mocks.MockMethods.MockMethod, null);
+            Assert.IsTrue(command.CanExecute(null));
+        }
+
+        [TestMethod]
+        public void CanExecuteNonNullTestSuccess()
+        {
+            var command = new Kinugasa.Mvvm.DelegateCommand(Mocks.MockMethods.MockMethod, Mocks.MockMethods.MockMethodReturnTrue);
+            var obj = new object();
+            Assert.IsTrue(command.CanExecute(obj));
+        }
+
+        [TestMethod]
+        public void CanExecuteNonNullTestFaild()
+        {
+            var command = new Kinugasa.Mvvm.DelegateCommand(Mocks.MockMethods.MockMethod, Mocks.MockMethods.MockMethodReturnFalse);
+            var obj = new object();
+            Assert.IsFalse(command.CanExecute(obj));
+        }
+
+        [TestMethod]
+        public void ExecuteTestSuccess()
+        {
+            var command = new Kinugasa.Mvvm.DelegateCommand(Mocks.MockMethods.MockMethod);
+            var po = new PrivateObject(command);
+            Assert.IsTrue(po.GetField("_execute").Equals((Action)Mocks.MockMethods.MockMethod));
+        }
+
+        [TestMethod]
+        public void ExecuteTestFaild()
+        {
+            var command = new Kinugasa.Mvvm.DelegateCommand(Mocks.MockMethods.MockMethod);
+            var po = new PrivateObject(command);
+            Assert.IsFalse(po.GetField("_execute").Equals((Action)Mocks.MockMethods.MockMethod2));
+        }
+    }
+
+    [TestClass]
+    public class DelegateCommandHasArgumentTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(System.ArgumentException))]
+        public void ArgumentExceptionTest()
+        {
+            var command = new Kinugasa.Mvvm.DelegateCommand<int>(null);
+        }
+
+        [TestMethod]
+        public void CanExecuteNullTest()
+        {
+            var command = new Kinugasa.Mvvm.DelegateCommand<int>(Mocks.MockMethods.MockMethodHasArgument, null);
+            Assert.IsTrue(command.CanExecute(null));
+        }
+
+        [TestMethod]
+        public void CanExecuteNonNullTestSuccess()
+        {
+            //HACK : Need brush up.
+            var command = new Kinugasa.Mvvm.DelegateCommand<int>(Mocks.MockMethods.MockMethodHasArgument, Mocks.MockMethods.MockMethodReturnTrue);
+            Assert.IsTrue(command.CanExecute(1));
+        }
+
+        [TestMethod]
+        public void CanExecuteNonNullTestFaild()
+        {
+            //HACK : Need brush up.
+            var command = new Kinugasa.Mvvm.DelegateCommand<int>(Mocks.MockMethods.MockMethodHasArgument, Mocks.MockMethods.MockMethodReturnFalse);
+            var obj = new object();
+            Assert.IsFalse(command.CanExecute(1));
+        }
+
+        [TestMethod]
+        public void ExecuteTestSuccess()
+        {
+            var command = new Kinugasa.Mvvm.DelegateCommand<int>(Mocks.MockMethods.MockMethodHasArgument);
+            var po = new PrivateObject(command);
+            Assert.IsTrue(po.GetField("_execute").Equals((Action<int>)Mocks.MockMethods.MockMethodHasArgument));
+        }
+
+        [TestMethod]
+        public void ExecuteTestFaild()
+        {
+            var command = new Kinugasa.Mvvm.DelegateCommand<int>(Mocks.MockMethods.MockMethodHasArgument);
+            var po = new PrivateObject(command);
+            Assert.IsFalse(po.GetField("_execute").Equals((Action)Mocks.MockMethods.MockMethod2));
+        }
+    }
+}
---
+++ Test/Mvvm/Mvvm.Test/Mocks/DelegateCommandMock.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mvvm.Test.Mocks
+{
+    public class DelegateCommandMock : Kinugasa.Mvvm.DelegateCommand
+    {
+        public DelegateCommandMock(Action execute) : base(execute, () => true)
+        {
+
+        }
+
+        public DelegateCommandMock(Action execute, Func<bool> canExecute) : base(execute, canExecute)
+        {
+
+        }
+    }
+
+    public class DelegateCommandMock<T> : Kinugasa.Mvvm.DelegateCommand<T>
+    {
+        public DelegateCommandMock(Action<T> execute) : base(execute, (O) => true)
+        {
+
+        }
+
+        public DelegateCommandMock(Action<T> execute, Func<T, bool> canExecute) : base(execute, canExecute)
+        {
+
+        }
+    }
+
+    internal static class MockMethods
+    {
+        internal static void MockMethod()
+        {
+
+        }
+
+        internal static void MockMethod2()
+        {
+
+        }
+
+        internal static bool MockMethodReturnTrue()
+        {
+            return true;
+        }
+
+        internal static bool MockMethodReturnFalse()
+        {
+            return false;
+        }
+
+        internal static void MockMethodHasArgument(int x)
+        {
+
+        }
+
+        internal static void MockMethodHasArgument2(int x)
+        {
+
+        }
+
+        internal static bool MockMethodReturnTrue(int x)
+        {
+            return true;
+        }
+
+        internal static bool MockMethodReturnFalse(int x)
+        {
+            return false;
+        }
+    }
+}
