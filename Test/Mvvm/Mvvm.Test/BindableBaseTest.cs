751359adc6db24092c6bc1df91149ee932dca07d
Yoshinori.N <nayo-msoft@outlook.jp>
2016-07-26 23:49:38 +0900

    (Issue #10) : Add 'Mvvm.Test' project and create 'BindableBase' class test code.

---
+++ Test/Mvvm/Mvvm.Test/BindableBaseTest.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Mvvm.Test
+{
+    [TestClass]
+    public class BindableBaseTest
+    {
+        [TestMethod]
+        public void MockIntTest()
+        {
+            var mockViewModel = new Mocks.MockViewModel();
+            mockViewModel.MockInt = 1;
+
+            Assert.AreEqual(1, mockViewModel.MockInt);
+        }
+
+        [TestMethod]
+        public void MockStringTest()
+        {
+            var mockViewModel = new Mocks.MockViewModel();
+            mockViewModel.MockString = "test";
+
+            Assert.AreEqual("test", mockViewModel.MockString);
+        }
+
+        [TestMethod]
+        public void MockOvservableCollectionTest()
+        {
+            var mockViewModel = new Mocks.MockViewModel();
+            mockViewModel.MockObservalCollection.Add(new Mocks.MockModel { MockInt = 1, MockString = "test"});
+            mockViewModel.MockObservalCollection.Add(new Mocks.MockModel { MockInt = 2, MockString = "test2" });
+
+            Assert.AreEqual(1, mockViewModel.MockObservalCollection[0].MockInt);
+            Assert.AreEqual("test", mockViewModel.MockObservalCollection[0].MockString);
+            Assert.AreEqual(2, mockViewModel.MockObservalCollection[1].MockInt);
+            Assert.AreEqual("test2", mockViewModel.MockObservalCollection[1].MockString);
+        }
+
+        [TestMethod]
+        public void OnPropertyChangedTestSuccess()
+        {
+            var mockViewModel = new Mocks.MockViewModel();
+
+            bool invoked = false;
+            mockViewModel.PropertyChanged += (o, e) => { if (e.PropertyName.Equals("MockInt")) invoked = true; };
+            mockViewModel.MockInt = 1;
+
+            Assert.IsTrue(invoked);
+        }
+
+        [TestMethod]
+        public void OnPropertyChangedPropertyNameTestSuccess()
+        {
+            var mockViewModel = new Mocks.MockViewModel();
+
+            bool invoked = false;
+            mockViewModel.PropertyChanged += (o, e) => { if (e.PropertyName.Equals("MockProperty")) invoked = true; };
+            mockViewModel.InvokeOnPropertyChanged();
+
+            Assert.IsTrue(invoked);
+        }
+
+        [TestMethod]
+        public void OnPropertyChangedPropertyNameTestFaild()
+        {
+            var mockViewModel = new Mocks.MockViewModel();
+
+            bool invoked = false;
+            mockViewModel.PropertyChanged += (o, e) => { if (e.PropertyName.Equals("MockFaild")) invoked = true; };
+            mockViewModel.InvokeOnPropertyChanged();
+
+            Assert.IsFalse(invoked);
+        }
+    }
+}
---
+++ Test/Mvvm/Mvvm.Test/Mocks/MockViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Mvvm.Test.Mocks
+{
+    public class MockViewModel : Kinugasa.Mvvm.BindableBase
+    {
+        private int _mockInt;
+
+        public int MockInt
+        {
+            get { return this._mockInt; }
+            set
+            {
+                this.SetProperty(ref this._mockInt, value);
+            }
+        }
+
+        private string _mockString;
+
+        public string MockString
+        {
+            get { return this._mockString; }
+            set
+            {
+                this.SetProperty(ref this._mockString, value);
+            }
+        }
+
+        private object MockProperty { get; set; }
+
+        private ObservableCollection<Mocks.MockModel> _mockObservalCollection = new ObservableCollection<MockModel>();
+
+        public ObservableCollection<Mocks.MockModel> MockObservalCollection
+        {
+            get { return this._mockObservalCollection; }
+            set
+            {
+                this.SetProperty(ref this._mockObservalCollection, value);
+            }
+        }
+
+        internal void InvokeOnPropertyChanged()
+        {
+            this.OnPropertyChanged(nameof(this.MockProperty));
+        }
+    }
+}
