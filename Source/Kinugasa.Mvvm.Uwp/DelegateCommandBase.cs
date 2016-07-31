8872f89effde69db72006947fa773312a6221ce2
Yoshinori.N <nayo-msoft@outlook.jp>
2016-07-31 23:03:49 +0900

    (Issue #52) : Create 'Kinugasa.Mvvm.Uwp' projects.

---
+++ Source/Kinugasa.Mvvm.Uwp/DelegateCommandBase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Kinugasa.Mvvm.uwp
+{
+    /// <summary>
+    /// Abstract class for implement command class. 
+    /// </summary>
+    public abstract class DelegateCommandBase : ICommand
+    {
+        /// <summary>
+        /// When call RaiseCanExecuteChanged method.
+        /// </summary>
+        public event EventHandler CanExecuteChanged;
+
+        /// <summary>
+        /// Call the RaiseCanExecuteChanged method.
+        /// </summary>
+        public void RaiseCanExecuteChanged()
+        {
+            var handler = CanExecuteChanged;
+            if (handler != null)
+            {
+                handler(this, EventArgs.Empty);
+            }
+        }
+
+        public abstract void Execute(object parameter);
+
+        public abstract bool CanExecute(object parameter);
+    }
+}
