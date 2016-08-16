70929f3dc4c343fae99d6e7fcd63413e7fc7093a
Yoshinori.N <nayo-msoft@outlook.jp>
2016-08-17 00:53:42 +0900

    Kinugasa.UI v1.0.0

--- Source/Shared/UI/Kinugasa.UI/Properties/AssemblyInfo.cs
+++ Source/Shared/UI/Kinugasa.UI/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Kinugasa.UI")]
-[assembly: AssemblyCopyright("Copyright (C)  2016")]
+[assembly: AssemblyCopyright("Copyright ©Yoshinori.N 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("ja")]
@@ -26,5 +26,6 @@ using System.Runtime.InteropServices;
 // すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
