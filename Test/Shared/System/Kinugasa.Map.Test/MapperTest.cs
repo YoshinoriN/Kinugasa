1e4fabc53004c73f07b1b3fcc465953cd5af3bed
YoshinoriN <nayo-msoft@outlook.jp>
2016-08-30 01:23:10 +0900

    (Issue #104) : Create 'Kinugasa.Map' project
    * To map same properties value between class A and class B.
    * To map properties using by attribute.

---
+++ Test/Shared/System/Kinugasa.Map.Test/MapperTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Kinugasa.Map.Test.Models;
+
+namespace Kinugasa.Map.Test
+{
+    [TestClass]
+    public class MapperTest
+    {
+        [TestMethod]
+        public void MapTest()
+        {
+            var a = new ClassA();
+            var b = new ClassB();
+            Mapper.Map(ref a, b);
+
+            Assert.AreEqual(a.StringA, b.StringA);
+            Assert.IsNull(a.StringB);
+            Assert.AreEqual(a.IntA, b.IntA);
+            Assert.AreEqual(a.IntB, 0);
+            Assert.AreEqual(a.StringList, b.StringList);
+            Assert.AreEqual(a.AttributeA,b.AttributeA);
+        }
+
+        [TestMethod]
+        public void MapAttributeTest()
+        {
+            var a = new ClassA();
+            var b = new ClassB();
+            Mapper.AttributeMap(ref a, b);
+
+            Assert.IsNull(a.StringA);
+            Assert.AreEqual(a.IntB, 0);
+            Assert.AreEqual(a.AttributeA, b.AttributeB);
+            Assert.AreEqual(a.AttributeB, b.AttributeA);
+            Assert.AreEqual(a.AttributeInt, b.AttributeInt2);
+            Assert.AreEqual(a.AttributeInt2, b.AttributeInt);
+        }
+    }
+}
---
+++ Test/Shared/System/Kinugasa.Map.Test/Models/ClassA.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using Kinugasa.Map.Attributes;
+
+namespace Kinugasa.Map.Test.Models
+{
+    public class ClassA
+    {
+        public string StringA { get; set; } = null;
+
+        public string StringB { get; set; } = null;
+
+        public int IntA { get; set; } = 0;
+
+        public int IntB { get; set; } = 0;
+
+        public List<string> StringList { get; set; } = new List<string>();
+
+        [MapAttribute("AttributeB")]
+        public string AttributeA { get; set; }
+
+        [MapAttribute("AttributeA")]
+        public string AttributeB { get; set; }
+
+        [MapAttribute("AttributeInt2")]
+        public int AttributeInt { get; set; } 
+
+        [MapAttribute("AttributeInt")]
+        public int AttributeInt2 { get; set; }
+    }
+}
