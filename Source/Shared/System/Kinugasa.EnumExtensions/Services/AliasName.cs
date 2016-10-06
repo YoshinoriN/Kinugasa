b134e44830564e4b0eb4888355d3877f0e7bab2d
YoshinoriN <nayo-msoft@outlook.jp>
2016-10-06 23:31:05 +0900

    (Issue #117) : Apply constraint to get alias name function

--- Source/Shared/System/Kinugasa.EnumExtensions/Services/AliasName.cs
+++ Source/Shared/System/Kinugasa.EnumExtensions/Services/AliasName.cs
@@ -18,6 +18,7 @@ namespace Kinugasa.EnumExtensions.Services
         /// <param name="source">Enum value</param>
         /// <returns><see cref="AliasAttribute.AliasName"></returns>
         public static string GetAlias<T>(this T source)
+            where T : struct
         {
             var attribute = source.GetType().GetRuntimeField(source.ToString()).GetCustomAttribute(typeof(AliasAttribute), false);
             return attribute == null ? source.ToString() : ((AliasAttribute)attribute).AliasName;
